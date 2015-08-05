a2f8ab8235535b74df892916fda46f9eb26866b6
Jack Wilsdon <jack.wilsdon2@gmail.com>
2015-08-05 21:26:26 +0100

    Add login method

---
+++ lib/steamweblogin.rb
@@ -0,0 +1,19 @@
+require_relative './ext/hash.rb'
+
+require_relative './errors.rb'
+require_relative './endpoint.rb'
+require_relative './jsonendpoint.rb'
+
+require_relative './endpoints/getrsakey.rb'
+require_relative './endpoints/dologin.rb'
+
+require_relative './connection.rb'
+
+module SteamWeb
+  def self.login(username, password, **options)
+    rsa_key = Endpoints::GetRSAKeyEndpoint.new.request username
+    transfer = Endpoints::DoLoginEndpoint.new.request username, password, rsa_key, options
+
+    transfer
+  end
+end
